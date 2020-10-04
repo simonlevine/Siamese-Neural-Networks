3afba387d4e430908347e905962f4cd6bb81ef9f
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2020-10-04 09:03:00 -0700

    Update data.py

--- data.py
+++ data.py
@@ -9,93 +9,69 @@ import torch
 import codecs
 import torch.utils.data as data
 import torch
-from torchvision.datasets.mnist import *
+from torchvision.datasets import ImageFolder
 
-class MNIST(VisionDataset):
-    """`MNIST <http://yann.lecun.com/exdb/mnist/>`_ Dataset.
-
+class FaceData(VisionDataset):
+    '''
     Args:
         root (string): Root directory of dataset where ``MNIST/processed/training.pt``
             and  ``MNIST/processed/test.pt`` exist.
-        train (bool, optional): If True, creates dataset from ``training.pt``,
-            otherwise from ``test.pt``.
-        download (bool, optional): If true, downloads the dataset from the internet and
-            puts it in root directory. If dataset is already downloaded, it is not
-            downloaded again.
+        train (bool, optional): If True, creates train dataset from ,
+            otherwise test.
+
         transform (callable, optional): A function/transform that  takes in an PIL image
             and returns a transformed version. E.g, ``transforms.RandomCrop``
         target_transform (callable, optional): A function/transform that takes in the
             target and transforms it.
-    """
+    '''
 
-    resources = [
-        ("http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz", "f68b3c2dcbeaaa9fbdd348bbdeb94873"),
-        ("http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz", "d53e105ee54ea40749a09fcbcd1e9432"),
-        ("http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz", "9fb629c4189551a2d022fa330f9573f3"),
-        ("http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz", "ec29112dd5afa0611ce80d1b7f02629c")
-    ]
 
     training_file = 'training.pt'
     test_file = 'test.pt'
     classes = ['0 - zero', '1 - one', '2 - two', '3 - three', '4 - four',
                '5 - five', '6 - six', '7 - seven', '8 - eight', '9 - nine']
 
-    @property
-    def train_labels(self):
-        warnings.warn("train_labels has been renamed targets")
-        return self.targets
+    hparams = {
+            'train_data_path' : '../data/classification_data/train_data/',
+            'test_data_path'  : '../data/classification_data/test_data/',
+            'val_data_path'   : '../data/classification_data/val_data/', 
+    }
 
-    @property
-    def test_labels(self):
-        warnings.warn("test_labels has been renamed targets")
-        return self.targets
+    self.train_data_path = hparams.get('train_data_path', None)
+    self.test_data_path = hparams.get('test_data_path', None)
+    self.val_data_path = hparams.get('val_data_path', None)
 
-    @property
-    def train_data(self):
-        warnings.warn("train_data has been renamed data")
-        return self.data
 
-    @property
-    def test_data(self):
-        warnings.warn("test_data has been renamed data")
-        return self.data
-
-    def __init__(self, root, train=True, transform=None, target_transform=None,
-                 download=False):
-        super(MNIST, self).__init__(root, transform=transform,
+    def __init__(self, root, train=True, transform=None, target_transform=None):
+        super(FaceData, self).__init__(root, transform=transform,
                                     target_transform=target_transform)
+
         self.train = train  # training set or test set
 
-        if download:
-            self.download()
-
-        if not self._check_exists():
-            raise RuntimeError('Dataset not found.' +
-                               ' You can use download=True to download it')
 
         if self.train:
-            data_file = self.training_file
-            self.data, self.targets = torch.load(os.path.join(self.processed_folder, data_file))
+            self.dataset = ImageFolder(root=self.train_data_path)
+            # self.data, self.targets = torch.load(os.path.join(self.processed_folder, data_file))
         else:
-            data_file = self.test_file
-            self.data, self.targets = torch.load(os.path.join(self.processed_folder, data_file)) 
+            self.dataset = ImageFolder(root=self.test_data_path)
+            # self.data, self.targets = torch.load(os.path.join(self.processed_folder, data_file)) 
             indices = [3, 2, 1, 18, 6, 8, 11, 17, 61, 16] # Manually picked support set
-            self.support_set = np.stack([self.transform(Image.fromarray(self.data[x].numpy(), mode='L')) for x in indices])   
+            self.support_set = np.stack([self.transform(self.data[x][0]) for x in indices])   
 
     def __getitem__(self, index):
         image1_index = np.random.randint(0, len(self.data))
-        image1 = self.data[image1_index]
-        target1 = int(self.targets[image1_index])
+        image1,_ = self.dataset[image1_index]
+        target1 = int(self.dataset.targets[image1_index])
 
-        image2 = self.data[index]
-        target2 = int(self.targets[index])
+        image2,_ = self.dataset[index]
+        target2 = int(self.dataset.targets[index])
 
         target = 1.0 if target1 == target2 else 0.0
 
         # doing this so that it is consistent with all other datasets
         # to return a PIL Image
-        image1 = Image.fromarray(image1.numpy(), mode='L')
-        image2 = Image.fromarray(image2.numpy(), mode='L')
+        # image1 = Image.fromarray(image1.numpy(), mode='L')
+        # image2 = Image.fromarray(image2.numpy(), mode='L')
 
         if self.transform is not None:
             image1 = self.transform(image1)
@@ -104,15 +80,7 @@ class MNIST(VisionDataset):
         return image1, image2, target, target1
 
     def __len__(self):
-        return len(self.data)
-
-    @property
-    def raw_folder(self):
-        return os.path.join(self.root, self.__class__.__name__, 'raw')
-
-    @property
-    def processed_folder(self):
-        return os.path.join(self.root, self.__class__.__name__, 'processed')
+        return len(self.dataset)
 
     @property
     def class_to_idx(self):
